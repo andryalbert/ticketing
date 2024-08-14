4c0b8dc9400b40550bf28b2d6cad419d98d51976
andry <nambinintsoaa@gmail.com>
2024-08-14 14:04:41 +0300

    modification mapper

--- ticketing/src/main/java/com/demo/ticketing/utils/TicketMapper.java
+++ ticketing/src/main/java/com/demo/ticketing/utils/TicketMapper.java
@@ -2,7 +2,9 @@ package com.demo.ticketing.utils;
 
 import com.demo.ticketing.dto.TicketDto;
 import com.demo.ticketing.model.Ticket;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TicketMapper {
 
     public static Ticket convertTicketDtoToTicket(TicketDto ticketDto){
